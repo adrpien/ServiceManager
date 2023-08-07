56842dfc57a6f78cdfbc3cd3c02ca6cb12424345
Adrian Pienkowski <adrpien@gmail.com>
2023-08-07 19:45:19 +0200

    - Testing Room Database finished

--- app/src/androidTest/java/com/example/servicemanager/feature_inspections/data/local/InspectionDatabaseDaoTest.kt
+++ app/src/androidTest/java/com/example/servicemanager/feature_inspections/data/local/InspectionDatabaseDaoTest.kt
@@ -65,4 +65,69 @@ class InspectionDatabaseDaoTest {
         assertThat(result).contains(inspectionEntity)
     }
 
+    @Test
+    fun deleteInspection() = runTest {
+        val inspectionEntity = InspectionEntity(
+            inspectionId = "test",
+            hospitalId = "ExF6PByjkDrHSCTzvYz3",
+            ward = "AIT",
+            comment = "comment",
+            inspectionDate = "1683410400000",
+            recipient = "Kamila Wodnicka",
+            technicianId = "Hy0eROd4a8080VIk3wjy",
+            inspectionStateId = "69h8l32O9qGKIRud9eiD",
+            estStateId = "0jUOtDLuw8JUjgK5YpFW",
+            deviceName = "Pompa infuzyjna",
+            deviceManufacturer = "Ascor",
+            deviceModel = "AP14",
+            deviceSn = "2014/198/776",
+            deviceIn = "T-802/1976"
+        )
+        inspectionDatabaseDao.insertInspection(inspectionEntity)
+        inspectionDatabaseDao.deleteInspection(inspectionEntity.inspectionId)
+        val result = inspectionDatabaseDao.getInspectionList()
+        assertThat(result).doesNotContain(inspectionEntity)
+    }
+
+    @Test
+    fun getInspectionList() = runTest {
+        val inspectionEntity1 = InspectionEntity(
+            inspectionId = "test1",
+        )
+        val inspectionEntity2 = InspectionEntity(
+            inspectionId = "test2",
+        )
+        val inspectionEntity3 = InspectionEntity(
+            inspectionId = "test3",
+        )
+
+        inspectionDatabaseDao.insertInspection(inspectionEntity1)
+        inspectionDatabaseDao.insertInspection(inspectionEntity2)
+        inspectionDatabaseDao.insertInspection(inspectionEntity3)
+        val result = inspectionDatabaseDao.getInspectionList()
+        assertThat(result).containsExactly(
+            inspectionEntity1,
+            inspectionEntity2,
+            inspectionEntity3
+        )
+    }
+
+    @Test
+    fun deleteAllInspections() = runTest {
+        val inspectionEntity1 = InspectionEntity(
+            inspectionId = "test1",
+        )
+        val inspectionEntity2 = InspectionEntity(
+            inspectionId = "test2",
+        )
+        val inspectionEntity3 = InspectionEntity(
+            inspectionId = "test3",
+        )
+        inspectionDatabaseDao.insertInspection(inspectionEntity1)
+        inspectionDatabaseDao.insertInspection(inspectionEntity2)
+        inspectionDatabaseDao.insertInspection(inspectionEntity3)
+        inspectionDatabaseDao.deleteAllInspections()
+        val result = inspectionDatabaseDao.getInspectionList()
+        assertThat(result).isEmpty()
+    }
 }
