084c2ea9bc6256cfc4d2d42327c569b1171425e3
Adrian Pienkowski <adrpien@gmail.com>
2023-09-18 19:11:41 +0200

    migrating to kotlin DSL started

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+
+    repositories {
+        mavenCentral()
+        google()
+    }
+
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.14")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.android.tools.build:gradle:7.0.4")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
