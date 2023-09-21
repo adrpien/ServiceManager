d4c1646c5c232be43e35c99f721067409cf5916f
Adrian Pienkowski <adrpien@gmail.com>
2023-09-21 16:05:18 +0200

    multi module structure creating in progress

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.3.14")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-        classpath("com.android.tools.build:gradle:8.1.1")
+        //classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        //classpath("com.android.tools.build:gradle:8.1.1")
     }
 }
 
