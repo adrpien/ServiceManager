5d3408e724ab0ffec65428e7603a56b0415ce489
Adrian Pienkowski <adrpien@gmail.com>
2023-09-22 21:22:23 +0200

    implementing multi modules finished

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 }
 
 apply(from = "$rootDir/base-module.gradle")
+
 android {
     namespace = "com.example.core"
 }
--- feature_inspections/feature_inspections_presentation/build.gradle.kts
+++ feature_inspections/feature_inspections_presentation/build.gradle.kts
@@ -1,44 +1,38 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    `android-library`
+    `kotlin-android`
+    id("com.google.devtools.ksp") version "1.8.0-1.0.9"
 }
+
+apply(from = "$rootDir/compose-module.gradle")
 
 android {
     namespace = "com.example.feature_inspections_presentation"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
+dependencies{
+    implementation(project(Modules.core))
+    implementation(project(Modules.featureInspectionsDomain))
 
-dependencies {
+    // Glide
+    implementation(Glide.glide)
+    "kapt"(Glide.glideCompiler)
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Compose dependencies
+    implementation(Compose.ui)
+    implementation(Compose.uiToolingPreview)
+    implementation(Compose.material)
+    implementation(Compose.material3)
+    implementation(Compose.activityCompose)
+    implementation(Compose.viewModelCompose)
+    implementation(Compose.hiltNavigationCompose)
+    implementation(Compose.materialIconExtended)
+    implementation(Compose.swipeRefresh)
+    debugImplementation(Compose.uiTooling)
+
+    // Compose Nav Destinations
+    implementation (RaamCostaNavigation.composeDestinationsCore)
+    ksp(RaamCostaNavigation.ksp)
+
+    // Date AlertDialog
+    implementation(DateAlertDialog.dateTime)
 }
