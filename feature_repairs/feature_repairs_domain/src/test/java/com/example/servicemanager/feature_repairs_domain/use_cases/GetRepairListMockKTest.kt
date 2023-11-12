917912f45a2f05886a5bbbba334e805def977080
Adrian Pienkowski <adrpien@gmail.com>
2023-11-12 17:38:01 +0100

    - GetRepairListMockKTest.kt: tests finished

--- feature_repairs/feature_repairs_domain/src/test/java/com/example/servicemanager/feature_repairs_domain/use_cases/GetRepairListMockKTest.kt
+++ feature_repairs/feature_repairs_domain/src/test/java/com/example/servicemanager/feature_repairs_domain/use_cases/GetRepairListMockKTest.kt
@@ -1,7 +1,11 @@
 package com.example.servicemanager.feature_repairs_domain.use_cases
 
 import assertk.assertThat
+import assertk.assertions.contains
+import assertk.assertions.doesNotContain
+import assertk.assertions.hasSize
 import assertk.assertions.isEqualTo
+import assertk.assertions.size
 import com.example.core.util.Resource
 import com.example.core.util.ResourceState
 import com.example.feature_repairs_domain.RepairRepositoryFake
@@ -11,7 +15,10 @@ import com.example.servicemanager.feature_repairs_domain.util.RepairOrderMonoton
 import com.example.servicemanager.feature_repairs_domain.util.RepairOrderType
 import com.example.test.hospitalDluga
 import com.example.test.hospitalORSK
+import com.example.test.inspection
+import com.example.test.repairDiagnosed
 import com.example.test.repairFinished
+import com.example.test.repairRepaired
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.flow.first
@@ -36,24 +43,26 @@ class GetRepairListMockKTest {
     private lateinit var updateRepair: UpdateRepair
     private lateinit var getRepair: GetRepair
 
+
     private val repair1 = repairFinished(
         repairId = "1",
         hospitalId = hospitalORSK().hospitalId,
         repairingDate = LocalDate.of(2022, 4, 8).toEpochDay().toString(),
+        comment = "test_comment"
     )
-    private val repair2 = repairFinished(
+    private val repair2 = repairRepaired(
         repairId = "2",
         hospitalId = hospitalDluga().hospitalId,
         repairingDate = LocalDate.of(2023, 9, 21).toEpochDay().toString(),
     )
-    private val repair3 = repairFinished(
+    private val repair3 = repairRepaired(
         repairId = "3",
         hospitalId = hospitalORSK().hospitalId,
         repairingDate = LocalDate.of(2020, 6, 1).toEpochDay().toString(),
     )
-    private val repair4 = repairFinished(
+    private val repair4 = repairDiagnosed(
         repairId = "4",
-        hospitalId = hospitalDluga().hospitalId,
+        hospitalId = hospitalORSK().hospitalId,
         repairingDate = LocalDate.of(2023, 2, 24).toEpochDay().toString(),
     )
 
@@ -87,17 +96,104 @@ class GetRepairListMockKTest {
             emit(Resource(ResourceState.SUCCESS, repairListTest))
         }
 
-
         val result = getRepairList(
             repairOrderType = RepairOrderType.Date(RepairOrderMonotonicity.Ascending)
         ).first { it.resourceState == ResourceState.SUCCESS }
 
         val data: List<Repair> = result.data ?: emptyList()
 
-        assertThat(data[0].repairId).isEqualTo("3")
+        assertThat(data[0].repairId).isEqualTo("4")
         assertThat(data[1].repairId).isEqualTo("1")
         assertThat(data[2].repairId).isEqualTo("4")
         assertThat(data[3].repairId).isEqualTo("2")
+    }
 
+    @Test
+    fun `repairOrderType argument properly order by state`() = runBlocking {
+        coEvery {
+            repairRepositoryMockK.getRepairList()
+        } returns flow {
+            emit(Resource(ResourceState.SUCCESS, repairListTest))
+        }
+        coEvery {
+            repairRepositoryMockK.getRepairListFromLocal()
+        } returns flow {
+            emit(Resource(ResourceState.SUCCESS, repairListTest))
+        }
+
+        val result = getRepairList(
+            repairOrderType = RepairOrderType.State(RepairOrderMonotonicity.Ascending)
+        ).first { it.resourceState == ResourceState.SUCCESS }
+
+        val data: List<Repair> = result.data ?: emptyList()
+
+        assertThat(data[0].repairId).isEqualTo("1")
+        assertThat(data[3].repairId).isEqualTo("4")
+    }
+
+    @Test
+    fun `repairOrderType argument properly order by hospital`() = runBlocking {
+        coEvery {
+            repairRepositoryMockK.getRepairList()
+        } returns flow {
+            emit(Resource(ResourceState.SUCCESS, repairListTest))
+        }
+        coEvery {
+            repairRepositoryMockK.getRepairListFromLocal()
+        } returns flow {
+            emit(Resource(ResourceState.SUCCESS, repairListTest))
+        }
+
+        val result = getRepairList(
+            repairOrderType = RepairOrderType.Hospital(RepairOrderMonotonicity.Ascending)
+        ).first { it.resourceState == ResourceState.SUCCESS }
+
+        val data: List<Repair> = result.data ?: emptyList()
+
+        assertThat(data[0].repairId).isEqualTo("2")
+    }
+
+    @Test
+    fun `hospitalFilter argument properly filters outrepairList`() = runBlocking {
+        coEvery {
+            repairRepositoryMockK.getRepairList()
+        } returns flow {
+            emit(Resource(ResourceState.SUCCESS, repairListTest))
+        }
+        coEvery {
+            repairRepositoryMockK.getRepairListFromLocal()
+        } returns flow {
+            emit(Resource(ResourceState.SUCCESS, repairListTest))
+        }
+
+        val result = getRepairList(
+            hospitalFilter = hospitalDluga()
+        ).first { it.resourceState == ResourceState.SUCCESS }
+
+        val data: List<Repair> = result.data ?: emptyList()
+
+        assertThat(data).hasSize(1)
+    }
+
+    @Test
+    fun `searchQuery argument properly filters out repairList`() = runBlocking {
+        coEvery {
+            repairRepositoryMockK.getRepairList()
+        } returns flow {
+            emit(Resource(ResourceState.SUCCESS, repairListTest))
+        }
+        coEvery {
+            repairRepositoryMockK.getRepairListFromLocal()
+        } returns flow {
+            emit(Resource(ResourceState.SUCCESS, repairListTest))
+        }
+
+        val result = getRepairList(
+            searchQuery = "test_comment"
+        ).first { it.resourceState == ResourceState.SUCCESS }
+
+        val data: List<Repair> = result.data ?: emptyList()
+
+        assertThat(data).hasSize(1)
     }
 }
